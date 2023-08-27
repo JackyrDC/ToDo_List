4b04eac039f448b0187c6b244351e5fa163fd3ce
JackyrDC <djackyrcalix@gmail.com>
2023-08-27 14:44:55 -0600

    Version a presentar.
    Funciona la parte lógica, pero faltan algunos retoques en la parte visual, especialmente con los botones.

---
+++ src/components/Items.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="container" style="list-style-type: none; padding-left: 0;">
+      <div class="rounded-pill" :class="{'bg-success': task.completed, 'bg-danger': !task.completed}">
+        <span>{{ task.text }}</span>
+        <button clas="btn btn-dark" @click="removeTask">Eliminar</button>
+      </div>
+      <div>
+        <input type="checkbox" v-model="task.completed" />
+        <span :class="{'text-success': task.completed, 'text-danger': !task.completed}">
+          {{ task.completed ? 'Completada' : 'Pendiente' }}
+        </span>
+      </div>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: ['task'],
+    methods: {
+      removeTask() {
+        this.$emit('remove-task', this.task);
+      }
+    }
+  };
+  </script>
+  
