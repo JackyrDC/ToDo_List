1d08bdaeaa08e6e900508d0e1fb4da5f8901a6e7
JackyrDC <30815697+JackyrDC@users.noreply.github.com>
2023-10-30 00:14:12 -0600

    Update Items.vue

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,13 +1,13 @@
 <template>
-    <li class="container" style="list-style-type: none; padding-left: 0;">
+    <li class="container" style="list-style-type: none; padding-left: 10px;">
       <div class="rounded-pill" :class="{'bg-success': task.completed, 'bg-danger': !task.completed}">
         <span>{{ task.text }}</span>
-        <button @click="removeTask">Eliminar</button>
+        <button @click="removeTask" class="btn btn-outline-dark">Eliminar</button>
       </div>
       <div>
         <input type="checkbox" v-model="task.completed" />
         <span :class="{'text-success': task.completed, 'text-danger': !task.completed}">
-          {{ task.completed ? 'Completada' : 'Pendiente' }}
+          {{ task.completed ? 'Realizada' : 'Pendiente' }}
         </span>
       </div>
     </li>
