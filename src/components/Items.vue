a3ed2598992ca52334be09e1d53ce7886d3d8ee9
JackyrDC <30815697+JackyrDC@users.noreply.github.com>
2023-10-30 00:22:51 -0600

    Update Items.vue

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,15 +1,18 @@
 <template>
     <li class="container" style="list-style-type: none; padding-left: 10px;">
-      <div class="rounded-pill container-fluid" :class="{'bg-success': task.completed, 'bg-danger': !task.completed}">
-        <span class="col-3 col-sm-6 m-3">{{ task.text }}</span>
-        <button @click="removeTask" class="btn btn-outline-dark col-4 col-sm-3 m-3">Eliminar</button>
+      <section class="rounded-pill container-fluid" :class="{'bg-success': task.completed, 'bg-danger': !task.completed}">
+          <div>
+            <span class="col-3 col-sm-6 m-3">{{ task.text }}</span>
+            <button @click="removeTask" class="btn btn-outline-dark col-4 col-sm-3 m-3">Eliminar</button>
+          </div>
+          <div>
+            <input type="checkbox" v-model="task.completed" />
+            <span :class="{'text-success': task.completed, 'text-danger': !task.completed}">
+              {{ task.completed ? 'Realizada' : 'Pendiente' }}
+            </span>
       </div>
-      <div>
-        <input type="checkbox" v-model="task.completed" />
-        <span :class="{'text-success': task.completed, 'text-danger': !task.completed}">
-          {{ task.completed ? 'Realizada' : 'Pendiente' }}
-        </span>
-      </div>
+      </section>
+
     </li>
   </template>
   
