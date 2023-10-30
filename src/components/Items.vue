a4b90a1ef290a2772f98d14cc98d54ec20d0aae5
JackyrDC <30815697+JackyrDC@users.noreply.github.com>
2023-10-30 00:50:08 -0600

    Update Items.vue

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -4,7 +4,7 @@
           <div class="row p-3">
             <span class="col-3 col-sm-4 ml-3 mr-3 mt-3 mb-3">{{ task.text }}</span>
             <div class="col-4 col-sm-5"></div>  
-            <button @click="removeTask" class="btn btn-outline-dark col-3 col-sm-3 mt-3 mb-3">Eliminar</button>
+            <button @click="removeTask" class="btn btn-outline-dark rounded-pill col-3 col-sm-3 mt-3 mb-3">Eliminar</button>
           </div>
       </section>
           <div class="container">
