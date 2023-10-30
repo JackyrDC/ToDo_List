9b228cbc1bc54072c07de0b7625aeaddaa6ec36b
JackyrDC <30815697+JackyrDC@users.noreply.github.com>
2023-10-30 00:38:39 -0600

    Update Items.vue

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -2,12 +2,12 @@
     <li style="list-style-type: none; padding-left: 10px;">
       <section class="rounded-pill container-fluid" :class="{'bg-success': task.completed, 'bg-danger': !task.completed}">
           <div class="row">
-            <span class="col-3 col-sm-6 ml-3 mr-3 mt-3">{{ task.text }}</span>
-            <button @click="removeTask" class="btn btn-outline-dark col-4 col-sm-3">Eliminar</button>
+            <span class="col-3 col-sm-4 ml-3 mr-3 mt-3">{{ task.text }}</span>
+            <button @click="removeTask" class="btn btn-outline-dark col-3 col-sm-3">Eliminar</button>
           </div>
           <div class="row">
             <input type="checkbox" class="ml-3 mt-1" v-model="task.completed" />
-            <span class="mt-1 ml-1">{{ task.completed ? 'Realizada' : 'Pendiente' }}</span>
+            <span>{{ task.completed ? 'Realizada' : 'Pendiente' }}</span>
           </div>
       </section>
 
