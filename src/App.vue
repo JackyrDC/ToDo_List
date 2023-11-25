fb0733f5d9b8cb3e9446e0afcc617a9e61f50d62
JackyrDC <djackyrcalix@gmail.com>
2023-11-24 20:35:49 -0600

    fix: 🐛 changes in the App file

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,62 @@
       <button class="btn btn-light" @click="addTask">Añadir Tarea</button>
     </div>
   </div>
-
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import Items from './components/Items.vue';
+
+export default {
+  components: {
+    Items
+  },
+  //Defino el estado del componente
+  setup() {
+    //Añado un arreglo de tareas al almacenamiento local, en caso de no haberlas, crea un arreglo vacío
+    const tasks = ref(JSON.parse(localStorage.getItem('tasks')) || []);
+    //Variable que almacenará las nuevas tareas
+    const newTask = ref('');
+    //Función para agregar nuevas tareas. Sólo lo hará si el cuadro de texto no está vacio.
+    const addTask = () => {
+      if (newTask.value.trim() !== '') {
+        tasks.value.push({
+          id: tasks.value.length + 1,
+          text: newTask.value,
+          completed: false
+        });
+        newTask.value = '';
+        updateLocalStorage();
+      }
+    };
+
+    //Función de borrado de tareas
+    const removeTask = task => {
+      tasks.value = tasks.value.filter(item => item !== task);
+      updateLocalStorage();
+    };
+
+    //Función para cambiar el estado de las tareas entre completadas o pendientes.
+    const toggleTaskStatus = task => {
+      task.completed = !task.completed;
+      updateLocalStorage();
+    };
+
+    //Función que permite el agregado de nuevas tareas a la lista presionando enter en el textbox.
+    const addTaskOnEnter = () => {
+      addTask();
+    };
+
+    //Función que guarda el arreglo de tareas en el almacenamiento, usando para ello el formato JSON.
+    const updateLocalStorage = () => {
+      localStorage.setItem('tasks', JSON.stringify(tasks.value));
+    };
+
+    onMounted(() => {
+      updateLocalStorage();
+    });
+
+    return { tasks, newTask, addTask, removeTask, toggleTaskStatus, addTaskOnEnter };
+  }
+};
+</script>
